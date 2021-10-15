d1579e017d4f2b963f0712991a8d1d7b36114031
Kármán Zsombor <karman.zsombor@infohullam.hu>
2021-10-15 17:27:14 +0200

    create form validator

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from './../../services/api.service'
 import { AuthService } from './../../services/auth.service'
 import { RegisterService } from './../../services/register.service'
-import { NgForm, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import {NgForm, Validators, FormGroup, FormBuilder, FormControl} from '@angular/forms';
 import { Router } from '@angular/router';
 import { timer } from 'rxjs';
 
@@ -13,52 +13,68 @@ import { timer } from 'rxjs';
     styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public form: FormGroup;
 
     msg!: string;
     source = timer(2000);
 
-    isLogin: boolean = false
-    errorMessage!: string  
+    isLogin: boolean = false;
+    errorMessage!: string;
 
     constructor(
         private _api: ApiService,
         private _auth: AuthService,
         private _router: Router,
         private _regservice: RegisterService,
-
-        private form: FormGroup,
-        private formBuilder: FormBuilder,
-
-    ) { }
-
-    ngOnInit(){
-        this.isUserLogin(); 
+    ) {
+      this.buildAndValidateForm();
     }
 
-    onSubmit(form: NgForm) {
-        console.log('Your form data : ', form.value);
-        const user = form.value
+    get usernameInput() { return this.form.get('username'); }
+    get emailInput() { return this.form.get('email'); }
 
-        
-        this.form = this.formBuilder.group({
-            name: ['', Validators.required],
-            username: ['', Validators.required],
-            email: ['', Validators.required],
-            phone: ['', Validators.required],
-            password: ['', Validators.required],
+    ngOnInit(){
+        this.isUserLogin();
+    }
+
+    private buildAndValidateForm() {
+      this.form = new FormGroup({
+        name: new FormControl(null, [ Validators.required ]),
+        username: new FormControl(null, [ Validators.required ]),
+        email: new FormControl(null, [ Validators.required, Validators.email ]),
+        phone: new FormControl(null, [ Validators.required ]),
+        password: new FormControl(null, [ Validators.required ]),
+      });
+    }
+
+    onSubmit() {
+      if (this.form.invalid) {
+        // alert('invalid form');
+        this.errorMessage = 'Invalid form';
+        return;
+      }
+
+      const user = this.form.value;
+      console.log('Your form data : ', user);
+
+      this._regservice.register(user)
+        .then(response => {
+          console.log(response);
+          this.msg = 'A regisztráció sikeres volt';
+          // TODO: ide tehetsz késleltetést, de szerintem inkább a visszajelző abalknak kelle modalnak, vagy alertnek lennie
+          this._router.navigate(['kezdolap']).then();
+        })
+        .catch(error => {
+          console.error('A regisztráció sikertelen volt!');
+          console.error(error);
+          this.errorMessage = error.message;
         });
-        
-
-        this._regservice.register(user);
-
-        this.msg = "A regisztráció sikeres volt";
-        this.source.subscribe(ido => this._router.navigate(['kezdolap']));
     }
 
     isUserLogin() {
         if (this._auth.getUserDetails() != null) {
             this.isLogin = true;
         }
-    }      
+    }
 
 }
